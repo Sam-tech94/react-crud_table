b78dd597d1e321fc7a5724183d34fb0d835b62f9
Sam-tech94 <arman1993.samvel@gmail.com>
2022-12-01 20:59:08 +0400

    deleted node-modules file

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Modal.css";
 
-
 const Modal = ({ open, children, handleRemoveClick, setIsOpen }) => {
 	if (!open) return null
 
